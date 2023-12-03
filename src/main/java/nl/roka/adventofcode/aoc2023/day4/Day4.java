911ce3861fc941c5aff518676e2e7aa2c4f2aa41
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2023-12-03 17:26:09 +0100

    Day 4: preparations

---
+++ src/main/java/nl/roka/adventofcode/aoc2023/day4/Day4.java
@@ -0,0 +1,27 @@
+package nl.roka.adventofcode.aoc2023.day4;
+
+import nl.roka.adventofcode.aoc.Answer;
+import nl.roka.adventofcode.aoc.Day;
+import nl.roka.adventofcode.aoc.Runner;
+import nl.roka.adventofcode.aoc2023.AbstractDayPuzzle;
+
+public class Day4 extends AbstractDayPuzzle {
+
+  public static void main(String[] args) {
+    Runner.run(new Day4());
+  }
+
+  public Day4() {
+    super(new Day(4));
+  }
+
+  @Override
+  public Answer runSilver() {
+    return Answer.TBD;
+  }
+
+  @Override
+  public Answer runGold() {
+    return Answer.TBD;
+  }
+}
---
+++ src/test/java/nl/roka/adventofcode/aoc2023/day4/Day4Test.java
@@ -0,0 +1,20 @@
+package nl.roka.adventofcode.aoc2023.day4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import nl.roka.adventofcode.aoc.Answer;
+import org.junit.jupiter.api.Test;
+
+class Day4Test {
+
+  @Test
+  void runSilver() {
+    assertThat(new Day4().runSilver()).isEqualTo(Answer.of("TBD"));
+  }
+
+  @Test
+  void runGold() {
+    assertThat(new Day4().runGold()).isEqualTo(Answer.of("TBD"));
+  }
+}
