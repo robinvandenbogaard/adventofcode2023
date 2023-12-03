f85b61d880da6089e96e1fd156d378eef1530d33
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2023-12-03 15:21:10 +0100

    Add Point

---
+++ src/main/java/nl/roka/adventofcode/aoc/Point.java
@@ -0,0 +1,21 @@
+package nl.roka.adventofcode.aoc;
+
+import java.util.List;
+
+public record Point(int x, int y) {
+  public static Point of(int x, int y) {
+    return new Point(x, y);
+  }
+
+  public List<Point> adjecent() {
+    return List.of(
+        Point.of(x - 1, y - 1),
+        Point.of(x, y - 1),
+        Point.of(x + 1, y - 1),
+        Point.of(x - 1, y),
+        Point.of(x + 1, y),
+        Point.of(x - 1, y + 1),
+        Point.of(x, y + 1),
+        Point.of(x + 1, y + 1));
+  }
+}
