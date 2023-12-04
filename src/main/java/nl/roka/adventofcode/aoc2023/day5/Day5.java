242a3361b19d55ac53ce2d2d786b3bc0ccc33109
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2023-12-04 15:55:52 +0100

    Day 5: Preparations

---
+++ src/main/java/nl/roka/adventofcode/aoc2023/day5/Day5.java
@@ -0,0 +1,27 @@
+package nl.roka.adventofcode.aoc2023.day5;
+
+import nl.roka.adventofcode.aoc.Answer;
+import nl.roka.adventofcode.aoc.Day;
+import nl.roka.adventofcode.aoc.Runner;
+import nl.roka.adventofcode.aoc2023.AbstractDayPuzzle;
+
+public class Day5 extends AbstractDayPuzzle {
+
+  public static void main(String[] args) {
+    Runner.run(new Day5());
+  }
+
+  public Day5() {
+    super(new Day(5));
+  }
+
+  @Override
+  public Answer runSilver() {
+    return Answer.TBD;
+  }
+
+  @Override
+  public Answer runGold() {
+    return Answer.TBD;
+  }
+}
---
+++ src/test/java/nl/roka/adventofcode/aoc2023/day5/Day5Test.java
@@ -0,0 +1,19 @@
+package nl.roka.adventofcode.aoc2023.day5;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nl.roka.adventofcode.aoc.Answer;
+import org.junit.jupiter.api.Test;
+
+class Day5Test {
+
+  @Test
+  void runSilver() {
+    assertThat(new Day5().runSilver()).isEqualTo(Answer.of("TBD"));
+  }
+
+  @Test
+  void runGold() {
+    assertThat(new Day5().runGold()).isEqualTo(Answer.of("TBD"));
+  }
+}
