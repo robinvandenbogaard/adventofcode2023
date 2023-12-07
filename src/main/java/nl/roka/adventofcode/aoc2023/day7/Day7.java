4953c4622df166c5e40a4465652def80a02cd8f2
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2023-12-07 21:37:57 +0100

    Day 7: prep

---
+++ src/main/java/nl/roka/adventofcode/aoc2023/day7/Day7.java
@@ -0,0 +1,28 @@
+package nl.roka.adventofcode.aoc2023.day7;
+
+import nl.roka.adventofcode.aoc.puzzle.AbstractDayPuzzle;
+import nl.roka.adventofcode.aoc.puzzle.Answer;
+import nl.roka.adventofcode.aoc.puzzle.Day;
+import nl.roka.adventofcode.aoc.puzzle.Solutions;
+import nl.roka.adventofcode.aoc.runner.Runner;
+
+public class Day7 extends AbstractDayPuzzle {
+
+  public static void main(String[] args) {
+    Runner.run(new Day7());
+  }
+
+  public Day7() {
+    super(new Day(7), Solutions.none());
+  }
+
+  @Override
+  public Answer runSilver() {
+    return Answer.TBD;
+  }
+
+  @Override
+  public Answer runGold() {
+    return Answer.TBD;
+  }
+}
---
+++ src/test/java/nl/roka/adventofcode/aoc2023/day7/Day7Test.java
@@ -0,0 +1,18 @@
+package nl.roka.adventofcode.aoc2023.day7;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nl.roka.adventofcode.aoc.puzzle.Answer;
+import org.junit.jupiter.api.Test;
+
+class Day7Test {
+  @Test
+  void runSilver() {
+    assertThat(new Day7().runSilver()).isEqualTo(Answer.of("TBD"));
+  }
+
+  @Test
+  void runGold() {
+    assertThat(new Day7().runGold()).isEqualTo(Answer.of("TBD"));
+  }
+}
