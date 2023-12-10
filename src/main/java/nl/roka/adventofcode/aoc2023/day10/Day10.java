282bbf494790c8686a1978321443a95505594c54
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2023-12-10 20:02:13 +0100

    Day 10: prep

---
+++ src/main/java/nl/roka/adventofcode/aoc2023/day10/Day10.java
@@ -0,0 +1,25 @@
+package nl.roka.adventofcode.aoc2023.day10;
+
+import nl.roka.adventofcode.aoc.puzzle.AbstractDayPuzzle;
+import nl.roka.adventofcode.aoc.puzzle.Answer;
+import nl.roka.adventofcode.aoc.puzzle.Day;
+import nl.roka.adventofcode.aoc.puzzle.Solutions;
+
+public class Day10 extends AbstractDayPuzzle {
+
+  private static final Solutions solutions = Solutions.of("TBD", "TBD");
+
+  public Day10() {
+    super(new Day(10), solutions);
+  }
+
+  @Override
+  public Answer runSilver() {
+    return Answer.TBD;
+  }
+
+  @Override
+  public Answer runGold() {
+    return Answer.TBD;
+  }
+}
---
+++ src/test/java/nl/roka/adventofcode/aoc2023/day10/Day10Test.java
@@ -0,0 +1,20 @@
+package nl.roka.adventofcode.aoc2023.day10;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import nl.roka.adventofcode.aoc.puzzle.Answer;
+import org.junit.jupiter.api.Test;
+
+class Day10Test {
+
+  @Test
+  void runSilver() {
+    assertThat(new Day10().runSilver()).isEqualTo(Answer.TBD);
+  }
+
+  @Test
+  void runGold() {
+    assertThat(new Day10().runGold()).isEqualTo(Answer.TBD);
+  }
+}
