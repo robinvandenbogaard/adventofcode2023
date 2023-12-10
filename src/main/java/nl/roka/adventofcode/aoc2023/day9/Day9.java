044630b8454ece8021605228bf4b0bfae1e00100
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2023-12-10 08:43:21 +0100

    Day 9: prep

---
+++ src/main/java/nl/roka/adventofcode/aoc2023/day9/Day9.java
@@ -0,0 +1,21 @@
+package nl.roka.adventofcode.aoc2023.day9;
+
+import nl.roka.adventofcode.aoc.puzzle.AbstractDayPuzzle;
+import nl.roka.adventofcode.aoc.puzzle.Answer;
+import nl.roka.adventofcode.aoc.puzzle.Day;
+
+public class Day9 extends AbstractDayPuzzle {
+  public Day9() {
+    super(new Day(9));
+  }
+
+  @Override
+  public Answer runSilver() {
+    return Answer.TBD;
+  }
+
+  @Override
+  public Answer runGold() {
+    return Answer.TBD;
+  }
+}
---
+++ src/test/java/nl/roka/adventofcode/aoc2023/day9/Day9Test.java
@@ -0,0 +1,18 @@
+package nl.roka.adventofcode.aoc2023.day9;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nl.roka.adventofcode.aoc.puzzle.Answer;
+import org.junit.jupiter.api.Test;
+
+class Day9Test {
+  @Test
+  void runSilver() {
+    assertThat(new Day9().runSilver()).isEqualTo(Answer.TBD);
+  }
+
+  @Test
+  void runGold() {
+    assertThat(new Day9().runGold()).isEqualTo(Answer.TBD);
+  }
+}
