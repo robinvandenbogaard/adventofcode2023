31ebb5bcee9024866722777315a0c66a4cfd8893
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2023-12-10 22:31:45 +0100

    Day 10: solve silver -> 7097

--- src/main/java/nl/roka/adventofcode/aoc2023/day10/Day10.java
+++ src/main/java/nl/roka/adventofcode/aoc2023/day10/Day10.java
@@ -4,10 +4,15 @@ import nl.roka.adventofcode.aoc.puzzle.AbstractDayPuzzle;
 import nl.roka.adventofcode.aoc.puzzle.Answer;
 import nl.roka.adventofcode.aoc.puzzle.Day;
 import nl.roka.adventofcode.aoc.puzzle.Solutions;
+import nl.roka.adventofcode.aoc.runner.Runner;
 
 public class Day10 extends AbstractDayPuzzle {
 
-  private static final Solutions solutions = Solutions.of("TBD", "TBD");
+  private static final Solutions solutions = Solutions.of("7097", "TBD");
+
+  public static void main(String[] args) {
+    Runner.run(new Day10());
+  }
 
   public Day10() {
     super(new Day(10), solutions);
